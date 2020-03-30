3a6e7795ecff91c9a6ec94c5b6610238f8fe1518
kiranmahi2593 <34339294+kiranmahi2593@users.noreply.github.com>
2020-03-30 19:41:15 +0530

    Add files via upload

---
+++ class2_variables_Types_Fumctions.py
@@ -0,0 +1,139 @@
+
+
+#a="ufff"
+#print(type(a))
+
+#print(len(a))
+
+
+
+
+#d= True
+#print(type(d))
+
+#Type converision
+#int to float
+
+'''
+num=5
+new=float(5)
+
+print(num)
+print(new)
+'''
+
+'''
+#int to string
+num = 6
+string= str(num)
+
+print(num)
+print(string)
+'''
+
+'''
+#int to int
+num = 6
+string= int(num)
+
+print(num)
+print(string)
+'''
+
+'''
+#int to bool
+num=6
+new_bool=bool(num)
+print(num)
+print(new_bool)
+
+num1=7
+new_bool=bool(num1)
+print(num1)
+print(new_bool)
+'''
+
+'''
+#float to int
+float_1 = 9.2
+float_int = int(float_1)
+print(float_1)
+print(float_int)
+'''
+
+'''
+#float to float
+float1=7
+float2=float(float1)
+print(float1)
+print(float2)
+'''
+
+'''
+#float to str
+float=9.6
+float_str=str(float)
+print(float)
+print(float_str)
+print(type(float_str))
+'''
+
+'''
+#float to bool
+float=7.5
+float_bool=bool(float)
+print(float)
+print(float_bool)
+'''
+
+'''
+#str to int
+name="kiran"
+str_int=int(name)
+print(name)
+print(str_int)
+'''
+
+'''
+#str to float
+name="kiran"
+str_float=float(name)
+print(name)
+print(str_float)
+'''
+
+'''
+#str to bool
+name='kiran'
+str_bool=bool(name)
+print(name)
+print(str_bool)
+'''
+
+'''
+#bool to int
+bool=True
+bool_int=int(bool)
+print(bool_int)
+'''
+
+'''
+#bool to float
+bool = True
+bool_float=float(bool)
+print(bool_float)
+'''
+
+'''
+#bool to str
+bool= True
+bool_str=str(bool)
+print(bool_str)
+'''
+
+'''
+#bool to bool
+bool1= True
+bool1_bool=bool(bool1)
+print(bool1_bool)
+'''
