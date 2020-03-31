e82f564b7a64d7d03aa074fe2515b910b9bc8c97
kiranmahi2593 <34339294+kiranmahi2593@users.noreply.github.com>
2020-03-31 21:23:19 +0530

    Add files via upload

---
+++ class3_Input_funtion.py
@@ -0,0 +1,86 @@
+'''
+a = input()
+print(a)
+print(type(a))
+'''
+
+'''
+b=int(input())
+print(b)
+print(type(b))
+
+
+
+c=float(input())
+print(c)
+print(type(c))
+'''
+
+
+'''
+d=bool(input())
+print(d)
+print(type(d))
+'''
+
+'''
+e=str(input())
+print(e)
+print(type(e))
+'''
+
+'''
+f=b+c
+print(f)
+'''
+
+
+'''
+#Area of circle
+pi=3.14
+r=float(input())
+area=pi*(r*r)
+print(area)
+print(type(area))
+'''
+
+
+'''
+#Area of circle
+pi=3.14
+r=int(input())
+area=pi*(r*r)
+print(area)
+print(type(area))
+print(type(r))
+'''
+
+
+
+'''
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+
+
+print((a*a)-(b*2))
+print((a+b)*(a-b))
+print((a+b)*2)
+print((a*a)+(b*b)+(2*(a*b)))
+'''
+
+
+#Cylinder
+pi=3.14
+r=int(input("enter the radius"))
+h=int(input("enter the height"))
+area=pi*r*r*h
+print(area)
+print(type(r))
+print(type(h))
+print(type(area))
+
+      
+
+
+
+
